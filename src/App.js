be5c4e991c04780d68b1d87164fdc097f0bb7a86
Anuja Sharma <anujasharman02@gmail.com>
2024-02-20 21:53:19 +0530

    used to manipulate the entered text in a few possible ways

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar  from './componenets/Navbar';
+import TextFrom from './componenets/TextForm';
+import About from './componenets/About';
+import { useCallback, useState } from 'react';
+import Alert from './componenets/Alert';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
+  const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = useCallback((message, type) => {
+    setAlert({
+      msg: message,
+      type: type  
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500)
+  }, [setAlert])
+
+  const toggleMode = useCallback(() => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#042743'
+      showAlert("Dark mode enabled", "success")
+      document.title = "textUtils - Dark Mode"
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light mode enabled", "success")
+      document.title = "TextUtils - Light mode"
+    }
+  }, [mode])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar title = "TextUtils" mode = {mode} toggleMode={toggleMode}/>
+        <Alert alert={alert}/>
+        <div className="container my-3">
+          <Routes>
+            <Route exact path = "/about" element ={<About mode = {mode}/>}/>
+            <Route path="/" element={<TextFrom heading= "Enter the text to analyze" mode={mode} showAlert={showAlert}/>}/>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/componenets/Alert.js
@@ -0,0 +1,17 @@
+export default function Alert(props) {
+
+    const capitalize = (word) => {
+        const newWord = word.toLowerCase();
+        return newWord.charAt(0).toUpperCase() + newWord.slice(1); 
+    }
+
+    return(
+        <div style = {{height: '50px'}}>
+            {
+                props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+                <strong>{capitalize(props.alert.type)}:</strong> {props.alert.msg}
+                </div>
+            }
+        </div>
+    );
+}
